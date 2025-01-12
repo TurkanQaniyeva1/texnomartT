350f3c4f8e13de64b10f8197898ee2d71cd20a2b
Turkan Qeniyeva <turkanqnyeva0@gmail.com>
2025-01-12 17:30:35 +0400

    first commit

--- src/Pages/Addtocard/Addtocard.jsx
+++ src/Pages/Addtocard/Addtocard.jsx
@@ -11,49 +11,52 @@ const Addtocard = () => {
   };
 
   return (
+   
     <section className="addPage">
       <div className="add">
-        <h2>Səbət</h2>
+    <h2>Səbət</h2>
       </div>
-      <div className="cart-container">
-        <div className="cartTab">
-        <table className="cart-table">
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Product Name</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>Remove</th>
-            </tr>
-          </thead>
-          <tbody>
+          <div className="cart-container">
+      <div className="cartTab">
+        <div className="cart-table-wrapper">
+          <table className="cart-table">
+            <thead>
+              <tr>
+                <th style={{ height: '53px' }}>Product</th>
+                <th style={{ height: '53px' }}>Product Name</th>
+                <th style={{ height: '53px' }}>Quantity</th>
+                <th style={{ height: '53px' }}>Price</th>
+                <th style={{ height: '53px' }}>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
             {products.map((product) => (
-              <tr key={product.id}>
-                <td>
-                  <img src={product.image} alt={product.name} className="product-image" />
-                </td>
-                <td>{product.name}</td>
-                <td>
-                  <div className="quantity">
-                    <button type="button" className="qty-minus" onClick={() => updateQuantity(product.id, "decrease")}>-</button>
-                    <span>{product.quantity}</span>
+               <tr key={product.id}>
+                 <td>
+                   <img src={product.image} alt={product.name} className="product-image" />
+                 </td>
+                 <td>{product.name}</td>
+                 <td>
+                   <div className="quantity">
+                     <button type="button" className="qty-minus" onClick={() => updateQuantity(product.id, "decrease")}>-</button>
+                     <span>{product.quantity}</span>
                     <button type="button" className="qty-plus" onClick={() => updateQuantity(product.id, "increase")}>+</button>
                   </div>
-                </td>
+                 </td>
                 <td>{product.price}</td>
                 <td>
                   <button className="remove">X</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>       
+                 </td>
+               </tr>
+             ))}
+            </tbody>
+          </table>
+        </div>
         <div className="action">
           <a href="#" className="woo-shop-button"> < FaLongArrowAltLeft/> Back to Shop</a>
         </div>
-        </div>
-        <div className="summary-container">
+      </div>
+      <div className="summary-container">
           <div className="cart-summary">
             <div className="totals-title">Products in cart</div>
             <p>Price <span>₼</span></p>
@@ -74,7 +77,7 @@ const Addtocard = () => {
             </a>
           </div>
         </div>
-      </div>
+    </div>
     </section>
   );
 };
